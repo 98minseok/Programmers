19ff478ccc46f1b64199066436b9b662815db4d9
Kim minseok <80530044+98minseok@users.noreply.github.com>
2025-09-22 17:30:49 +0900

    [level 2] Title: 피보나치 수, Time: 1.99 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        
+        int num1 = 0;
+        int num2 = 1;
+        int num3 = 0;
+        
+        for(int i = 1 ; i < n ; i ++){
+            num3 = num1 + num2;
+            num1 = num2 % 1234567;
+            num2 = num3 % 1234567;
+        }
+        
+        return num3 % 1234567;
+    }
+}
